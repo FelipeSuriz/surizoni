02f82f52fc76af262a91d90dae91e33045847a81
Felipe Suriz <felipesuriz@hotmail.com>
2023-09-26 09:13:35 -0300

    Tentando importar o logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
 import styles from './Cardapio.module.scss';
+import logo from '../../assets/logo.svg';
 
 export default function Cardapio() {
-    return(
+    return (
         <main>
-            <nav className="menu">
-
+            <nav className={styles.menu}>
+                <img src={logo} alt='logo do surizoni' />
             </nav>
         </main>
     )
