82f323c4789148e1135ed63970e7ea0e68819f97
Felipe Suriz <felipesuriz@hotmail.com>
2023-10-05 10:54:09 -0300

    Criando o buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,10 @@
+import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import { useState } from "react";
 
 export default function Cardapio() {
+    const [busca, setBusca] = useState("");
     return (
         <main>
             <nav className={styles.menu}>
@@ -12,6 +15,13 @@ export default function Cardapio() {
                     A casa do rango e da massa!
                 </div>
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador
+                    busca={busca}
+                    setBusca={setBusca}
+                />
+            </section>
         </main>
     )
 }
