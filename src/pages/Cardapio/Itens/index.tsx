e9c5676a2db9e22a88b09000323a6950547ca0c7
Felipe Suriz <felipesuriz@hotmail.com>
2023-10-27 11:24:45 -0300

    Criando o ESLint

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -10,54 +10,54 @@ interface Props {
 }
 
 export default function Itens(props: Props) {
-    const [lista, setLista] = useState(cardapio);
-    const { busca, filtro, ordenador } = props;
+  const [lista, setLista] = useState(cardapio);
+  const { busca, filtro, ordenador } = props;
 
-    function testaBusca(title: string) {
-        const regex = new RegExp(busca, 'i');
-        return regex.test(title);
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if (filtro !== null) return filtro === id;
+    return true;
+  }
+
+  const ordenarPropriedadeCrescente = (
+    lista: typeof cardapio,
+    propriedade: 'size' | 'serving' | 'price'
+  ) => {
+    return lista.sort((a, b) => (a[propriedade] > b[propriedade] ? 1 : -1));
+  };
+
+  function ordenar(novaLista: typeof cardapio) {
+    switch (ordenador) {
+    case 'porcao':
+      return ordenarPropriedadeCrescente(novaLista, 'size');
+    case 'qtd_pessoas':
+      return ordenarPropriedadeCrescente(novaLista, 'serving');
+    case 'preco':
+      return ordenarPropriedadeCrescente(novaLista, 'price');
+    default:
+      return novaLista;
     }
+  }
 
-    function testaFiltro(id: number) {
-        if (filtro !== null) return filtro === id;
-        return true;
-    }
+  useEffect(() => {
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(ordenar(novaLista));
+  }, [busca, filtro, ordenador]);
 
-    const ordenarPropriedadeCrescente = (
-        lista: typeof cardapio,
-        propriedade: 'size' | 'serving' | 'price'
-    ) => {
-        return lista.sort((a, b) => (a[propriedade] > b[propriedade] ? 1 : -1));
-    };
-
-    function ordenar(novaLista: typeof cardapio) {
-        switch (ordenador) {
-            case 'porcao':
-                return ordenarPropriedadeCrescente(novaLista, 'size');
-            case 'qtd_pessoas':
-                return ordenarPropriedadeCrescente(novaLista, 'serving');
-            case 'preco':
-                return ordenarPropriedadeCrescente(novaLista, 'price');
-            default:
-                return novaLista;
-        }
-    }
-
-    useEffect(() => {
-        const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
-        setLista(ordenar(novaLista));
-    }, [busca, filtro, ordenador]);
-
-    return (
-        <div className={styles.itens}>
-            {lista.map((item) => (
-                <div>
-                    <Item
-                        key={item.id}
-                        {...item}
-                    />
-                </div>
-            ))}
+  return (
+    <div className={styles.itens}>
+      {lista.map((item) => (
+        <div>
+          <Item
+            key={item.id}
+            {...item}
+          />
         </div>
-    )
+      ))}
+    </div>
+  );
 }
